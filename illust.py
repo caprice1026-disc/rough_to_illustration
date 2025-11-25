86fbad61f0e67509dcb31806d2e8c502982bd330
Itani <omaewomiteru@gmail.com>
2025-11-26 00:48:40 +0900

    実際に呼び出すための関数のモックを追加

--- illust.py
+++ illust.py
@@ -21,15 +21,24 @@ https://ai.google.dev/gemini-api/docs/image-understanding?hl=ja
 '''
 image = Image.open("/path/to/cat_image.png") # 画像のパスを指定。修正予定
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash-image", # モデルは後で差し替えれるようにする、gemini-3-pro-image-previewを使用する予定
-    contents=[prompt, image],
-)
+def generate_image(
+    prompt: str, 
+    image: Image.Image, 
+    aspect_ratio: str | None = None,
+    resolution: str | None = None
+    ) -> types.Content:
+    '''
+    プロンプトと画像を使って画像生成APIを呼び出す関数
+    '''
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-image", # モデルは後で差し替えれるようにする、gemini-3-pro-image-previewを使用する予定
+        contents=[prompt, image],
+    )
 
-for part in response.parts:
-    if part.text is not None:
-        print(part.text)
-    elif part.inline_data is not None:
-        image = part.as_image()
-        image.save("generated_image.png")
+    for part in response.parts:
+        if part.text is not None:
+            print(part.text)
+        elif part.inline_data is not None:
+            image = part.as_image()
+            image.save("generated_image.png")
         
